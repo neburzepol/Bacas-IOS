d6ba008c3974fe1f178235b4c99ba599ee07b334
Ali Lopez Galaviz <lopez.galaviz@gmail.com>
2016-05-15 16:49:41 -0600

    Initial Commit

---
+++ Baccus/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Baccus
+//
+//  Created by Ali Lopez Galaviz on 15/05/16.
+//  Copyright © 2016 Agbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
