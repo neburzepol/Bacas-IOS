5e45fa30bfffcf3b5c90baa70de02b894a9a5d9d
Ali Lopez Galaviz <lopez.galaviz@gmail.com>
2016-05-15 17:30:10 -0600

    Agregamos linea para poder ver nuestra aplicación ya que por la version no podríamos desplegarla fácilmente

--- Baccus/AppDelegate.m
+++ Baccus/AppDelegate.m
@@ -16,9 +16,10 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];//acopla la pantalla
+    self.window.rootViewController = [[UIViewController alloc]init];
     // Override point for customization after application launch.
-    self.window.backgroundColor = [UIColor whiteColor];
+    self.window.backgroundColor = [UIColor orangeColor];//Asigna color a la pantallar
     [self.window makeKeyAndVisible];
     return YES;
 }
@@ -26,19 +27,23 @@
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    //La aplicacion dejara de ser la activa (Algun tipo de interrupcion como llamada telefonica
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    //Cuando una aplicacion pasa a backgroud, tienes 5 segundos para hacer algo, guardar datos, cerrar conexiones de redes, parar sonidos, pausar juegos etc.
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    //Volver a primer plano
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    //Ya esta en primer plano
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
