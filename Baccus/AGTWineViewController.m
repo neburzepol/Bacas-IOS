57a34197e278f83b1e98a94d02c3bf336bd2b0de
Ali Lopez Galaviz <lopez.galaviz@gmail.com>
2016-05-15 22:19:39 -0600

    Agregamos las propiedades y las unimos a nuestro controlador

--- Baccus/AGTWineViewController.m
+++ Baccus/AGTWineViewController.m
@@ -33,14 +33,9 @@
     
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+#pragma mark -actions
+-(IBAction)displayWeb:(id)sender{
+    NSLog(@"Go to %@", self.model.wineCompanyWeb);
 }
-*/
 
 @end
