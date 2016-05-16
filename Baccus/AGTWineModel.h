a6aa3795ceb4957abc16c93f4fffa1c622d90220
Ali Lopez Galaviz <lopez.galaviz@gmail.com>
2016-05-15 18:33:08 -0600

    Se han creado el método designado y método de conveniencia de nuestra aplicación.

---
+++ Baccus/AGTWineModel.h
@@ -0,0 +1,45 @@
+//
+//  AGTWineModel.h
+//  Baccus
+//
+//  Created by Ali Lopez Galaviz on 15/05/16.
+//  Copyright © 2016 Agbo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIkit.h>
+
+#define NO_RATING -1
+
+@interface AGTWineModel : NSObject
+
+@property (strong,nonatomic) NSString *type;
+@property (strong,nonatomic) UIImage *photo;
+@property (strong,nonatomic) NSURL *wineCompanyWeb;
+@property (strong,nonatomic) NSString *notes;
+@property (strong,nonatomic) NSString *origin;
+@property (nonatomic) int rating; //0 - 5
+@property (strong,nonatomic) NSArray *grapes;
+@property (strong,nonatomic) NSString *name;
+@property (strong,nonatomic) NSString *wineCompanyName;
+
+//Designado
+-(id)initWithName:(NSString*)aName
+  wineCompanyName:(NSString*) aWineCompanyName
+             type:(NSString*)aType
+           origin:(NSString*)aOrigin
+           grapes:(NSArray*)arrayOfGrapes
+   wineCompanyWeb:(NSURL*)aURL
+            notes:(NSString*)aNotes
+           rating:(int)aRating
+            photo:(UIImage*)aPhoto;
+
+-(id)initWithName:(NSString*)aName
+  wineCompanyName:(NSString*)aWineCompanyName
+             type:(NSString*)aType
+           origin:(NSString*) anOrigin;
+
+
+
+
+@end
---
+++ Baccus/AGTWineModel.m
@@ -0,0 +1,58 @@
+//
+//  AGTWineModel.m
+//  Baccus
+//
+//  Created by Ali Lopez Galaviz on 15/05/16.
+//  Copyright © 2016 Agbo. All rights reserved.
+//
+
+#import "AGTWineModel.h"
+
+@implementation AGTWineModel
+
+#pragma mark - init
+
+-(id)initWithName:(NSString*)aName
+  wineCompanyName:(NSString*)aWineCompanyName
+             type:(NSString*)aType
+           origin:(NSString*)aOrigin
+           grapes:(NSArray*)arrayOfGrapes
+   wineCompanyWeb:(NSURL*)aURL
+            notes:(NSString*)aNotes
+           rating:(int)aRating
+            photo:(UIImage*)aPhoto{
+    
+    if (self = [super init]) {
+        _name = aName;
+        _wineCompanyName = aWineCompanyName;
+        _type = aType;
+        _origin = aOrigin;
+        _grapes = arrayOfGrapes;
+        _wineCompanyWeb = aURL;
+        _notes = aNotes;
+        _rating = aRating;
+        _photo = aPhoto;
+    }
+    
+    return self;
+}
+
+-(id)initWithName:(NSString*)aName
+  wineCompanyName:(NSString*)aWineCompanyName
+             type:(NSString*)aType
+           origin:(NSString*)anOrigin{
+    
+    return [self initWithName:aName
+              wineCompanyName:aWineCompanyName
+                         type:aType
+                       origin:anOrigin
+                       grapes:nil
+               wineCompanyWeb:nil
+                        notes:nil
+                       rating:NO_RATING
+                        photo:nil];
+    
+}
+
+
+@end
