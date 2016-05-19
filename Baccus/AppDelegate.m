d6a0a81468ec09885b402f40b729001b532c4399
Ali Lopez Galaviz <lopez.galaviz@gmail.com>
2016-05-19 15:06:51 -0600

    Hemos hecho la combinación de NavigationController con un TabBarController.

--- Baccus/AppDelegate.m
+++ Baccus/AppDelegate.m
@@ -29,19 +29,47 @@
                                                  grapes:@[@"Mencía"]
                                          wineCompanyWeb:[NSURL URLWithString:@"http://www.dominiodetares.com/index.php/es/vinos/baltos/74-bembibrevinos"]
                                                   notes:@"Este vino muestra toda la complejidad y la elegancia de la variedad Mencía. En fase visual luce un color rojo picota muy cubierto con tonalidades violáceas en el menisco. En nariz aparecen recuerdos frutales muy intensos de frutas rojas (frambuesa, cereza) y una potente ciruela negra, así como tonos florales de la gama de las rosas y violetas, vegetales muy elegantes y complementarios, hojarasca verde, tabaco y maderas aromáticas (sándalo) que le brindan un toque ciertamente perfumado."
-                                                 rating:3
+                                                 rating:5
                                                   photo:[UIImage imageNamed:@"bembibre.jpg"]];
     
-    // Creamos los controlador
-    AGTWineViewController *wineVC = [[AGTWineViewController alloc]initWithModel:tintorro];
+    AGTWineModel *albarinno = [AGTWineModel wineWithName:@"Zárate"
+                                         wineCompanyName:@"Zárate"
+                                                    type:@"white"
+                                                  origin:@"Rias Bajas"
+                                                  grapes:@[@"Albariño"]
+                                          wineCompanyWeb:[NSURL URLWithString:@"http://www.albarino-zarate.com"]
+                                                   notes:@"El albariño Zarate es un vino blanco monovarietal que pertenece a la Denominación de Origen Rías Baixas. Considerado por la crítica especializada como uno de los grandes vinos blancos del mundo, el albariño ya es todo un mito."
+                                                  rating:4
+                                                   photo:[UIImage imageNamed:@"zarate.gif"]];
+    
+    AGTWineModel *champagne = [AGTWineModel wineWithName:@"Comtes de Champagne"
+                                         wineCompanyName:@"Champagne Taittinger"
+                                                    type:@"other"
+                                                  origin:@"Champagne"
+                                                  grapes:@[@"Chardonnay"]
+                                          wineCompanyWeb:[NSURL URLWithString:@"http://www.taittinger.fr"]
+                                                   notes:@"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac nunc purus. Curabitur eu velit mauris. Curabitur magna nisi, ullamcorper ac bibendum ac, laoreet et justo. Praesent vitae tortor quis diam luctus condimentum. Suspendisse potenti. In magna elit, interdum sit amet facilisis dictum, bibendum nec libero. Maecenas pellentesque posuere vehicula. Vivamus eget nisl urna, quis egestas sem. Vivamus at venenatis quam. Sed eu nulla a orci fringilla pulvinar ut eu diam. Morbi nibh nibh, bibendum at laoreet egestas, scelerisque et nisi. Donec ligula quam, semper nec bibendum in, semper eget dolor. In hac habitasse platea dictumst. Maecenas adipiscing semper rutrum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;"
+                                                  rating:5
+                                                   photo:[UIImage imageNamed:@"comtesDeChampagne.jpg"]];
+    
+    
+    // Creamos los controladores
+    AGTWineViewController *tintoVC = [[AGTWineViewController alloc]initWithModel:tintorro];
+    AGTWineViewController *blancoVC = [[AGTWineViewController alloc]initWithModel:albarinno];
+    AGTWineViewController *otroVC = [[AGTWineViewController alloc]initWithModel:champagne];
     //AGTWebViewController *webVC = [[AGTWebViewController alloc]initWithModel:tintorro];
     
-    // Creamos el combinador
-    UINavigationController *navVC = [[UINavigationController alloc]initWithRootViewController:wineVC];
+    // Creamos los navigation
+    UINavigationController *tintoNav = [[UINavigationController alloc]initWithRootViewController:tintoVC];
+    UINavigationController *blancoNav = [[UINavigationController alloc]initWithRootViewController:blancoVC];
+    UINavigationController *otroNav = [[UINavigationController alloc]initWithRootViewController:otroVC];
     
+    //Creamos el combinador
+    UITabBarController *tabVC = [[UITabBarController alloc]init];
+    tabVC.viewControllers = @[tintoNav,blancoNav,otroNav];
     
-    //Lo asignamos como controlador raiz
-    self.window.rootViewController = navVC;
+    //Lo asignamos como controlador Raiz
+    self.window.rootViewController = tabVC;
     
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor orangeColor];//Asigna color a la pantallar
